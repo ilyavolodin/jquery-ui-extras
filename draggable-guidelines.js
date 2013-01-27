c92dd324781d0b4484d2a8aa9f05f09f0ae31e9f
Ilya Volodin <ivolodin@vistaprint.com>
2013-01-26 20:51:18 -0500

    jQuery UI rotatable
    Initial implementation for jQuery UI rotatable. And some plugins for
    draggable.

---
+++ draggable-guidelines.js
@@ -0,0 +1,188 @@
+﻿$.ui.plugin.add("draggable", "guidelines", {
+    start: function (event, ui)
+    {
+        //format for snap guidelines is { position: x/y, snapSide: [top, right, bottom, left], element: $(), size: {top, left}, offset: {top, left} }
+        //format for snap margins is { position: x/y, snapSide: [top, right, bottom, left], element: $(), size: {top, left}, offset: {top, left} }
+
+        var i = $(this).data("draggable"), o = i.options;
+        if (typeof (o.guidelines.items) === "function")
+        {
+            i.items = o.guidelines.items.apply(this);
+        }
+        else if ($.isArray(o.guidelines.items))
+        {
+            i.items = o.guidelines.items;
+        }
+        if (typeof (o.guidelines.margins) === "function")
+        {
+            i.margins = o.guidelines.margins.apply(this);
+        }
+        else if ($.isArray(o.guidelines.margins))
+        {
+            i.margins = o.guidelines.margins;
+        }
+
+        i.dragElementSize = { width: $(this).width(), height: $(this).height() };
+    },
+    drag: function (event, ui)
+    {
+        if (event.shiftKey)
+        {
+            return;
+        }
+        var data = $(this).data("draggable");
+        var options = data.options;
+        var snapTolerance = options.guidelines.snapTolerance ? options.guidelines.snapTolerance : 2;
+        var currentItem = { top: ui.offset.top, right: data.dragElementSize.width + ui.offset.left, bottom: data.dragElementSize.height + ui.offset.top, left: ui.offset.left };
+        var sides = ["top", "right", "bottom", "left"];
+        var snaps = [];
+        for (var i = 0, l = sides.length; i < l; i++)
+        {
+            var sideSnaps = $.grep(data.items, function(item) {
+                return item.snapSide === sides[i];
+            });
+            var marginSnaps = $.grep(data.margins, function (item) {
+                return item.snapSide === sides[i];
+            });
+            var j, len;
+            for (j = 0, len = sideSnaps.length; j < len; j++)
+            {
+                if (Math.abs(currentItem[sides[i]] - sideSnaps[j].position) <= snapTolerance)
+                {
+                    //found a guideline for a snap
+                    switch (sides[i])
+                    {
+                        case "top":
+                            ui.position.top = data._convertPositionTo("relative", { top: sideSnaps[j].position, left: 0 }).top;
+                            break;
+                        case "bottom":
+                            ui.position.top = data._convertPositionTo("relative", { top: sideSnaps[j].position, left: 0 }).top - data.dragElementSize.height;
+                            break;
+                        case "right":
+                            ui.position.left = data._convertPositionTo("relative", { left: sideSnaps[j].position, top: 0 }).left - data.dragElementSize.width;
+                            break;
+                        case "left":
+                            ui.position.left = data._convertPositionTo("relative", { left: sideSnaps[j].position, top: 0 }).left;
+                            break;
+                        default:
+                            break;
+                    }
+                    snaps.push(sideSnaps[j]);
+                }
+            }
+
+            for (j = 0, len = marginSnaps.length; j < len; j++)
+            {
+                var oppositeSide = (i + 2) > 3 ? (i - 2) : (i + 2);
+                if (Math.abs(currentItem[sides[oppositeSide]] - marginSnaps[j].position) <= snapTolerance)
+                {
+                    if (sides[i] === "top" || sides[i] === "bottom")
+                    {
+                        if (Math.min(currentItem.right, marginSnaps[j].offset.left + marginSnaps[j].size.width) - Math.max(currentItem.left, marginSnaps[j].offset.left) <= 0)
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        if (Math.min(currentItem.bottom, marginSnaps[j].offset.top + marginSnaps[j].size.height) - Math.max(currentItem.top, marginSnaps[j].offset.top) <= 0)
+                        {
+                            continue;
+                        }
+                    }
+                    //found margin for a snap
+                    switch (sides[i])
+                    {
+                        case "top":
+                            ui.position.top = data._convertPositionTo("relative", { top: marginSnaps[j].position - snapTolerance, left: 0 }).top - data.dragElementSize.height;
+                            break;
+                        case "bottom":
+                            ui.position.top = data._convertPositionTo("relative", { top: marginSnaps[j].position + snapTolerance, left: 0 }).top;
+                            break;
+                        case "right":
+                            ui.position.left = data._convertPositionTo("relative", { left: marginSnaps[j].postion + snapTolerance, top: 0 }).left;
+                            break;
+                        case "left":
+                            ui.position.left = data._convertPositionTo("relative", { left: marginSnaps[j].position - snapTolerance, top: 0 }).left - data.dragElementSize.width;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        //clear old guides
+        $(".draggable-snap-guidelines").remove();
+
+        var draggableOffset = ui.helper.offset();
+
+        //render guides
+        $.each(snaps, function ()
+        {
+            var snappedToOffset = this.offset;
+            var snappedToSize = this.size;
+            var snappedToSides = { top: snappedToOffset.top, right: snappedToOffset.left + snappedToSize.width, bottom: snappedToOffset.top + snappedToSize.height, left: snappedToOffset.left };
+            var draggableSides = { top: draggableOffset.top, right: draggableOffset.left + data.dragElementSize.width, bottom: draggableOffset.top + data.dragElementSize.height, left: draggableOffset.left };
+            if (snappedToSides[this.snapSide] !== draggableSides[this.snapSide])
+            {
+                return;
+            }
+
+            var snapGuideline = $("<div class='draggable-snap-guidelines'></div>");
+            this.element.parent().append(snapGuideline);
+
+            var guidePosition;
+
+            switch (this.snapSide)
+            {
+                case "top":
+                    guidePosition = data._convertPositionTo("relative", { top: draggableOffset.top, left: Math.min(snappedToOffset.left + snappedToSize.width, draggableOffset.left + data.dragElementSize.width) });
+                    snapGuideline.css({
+                        "top": guidePosition.top,
+                        "left": guidePosition.left,
+                        "width": Math.abs(Math.max(snappedToOffset.left, draggableOffset.left) - Math.min(snappedToOffset.left + snappedToSize.width, draggableOffset.left + data.dragElementSize.width))
+                    });
+                    snapGuideline.addClass("draggable-snap-guidelines-horizontal");
+                    break;
+                case "bottom":
+                    guidePosition = data._convertPositionTo("relative", { top: draggableOffset.top + data.dragElementSize.height, left: Math.min(snappedToOffset.left + snappedToSize.width, draggableOffset.left + data.dragElementSize.width) });
+                    snapGuideline.css({
+                        "top": guidePosition.top,
+                        "left": guidePosition.left,
+                        "width": Math.abs(Math.max(snappedToOffset.left, draggableOffset.left) - Math.min(snappedToOffset.left + snappedToSize.width, draggableOffset.left + data.dragElementSize.width))
+                    });
+                    snapGuideline.addClass("draggable-snap-guidelines-horizontal");
+                    break;
+                case "left":
+                    guidePosition = data._convertPositionTo("relative", { top: Math.min(snappedToOffset.top + snappedToSize.height, draggableOffset.top + data.dragElementSize.height), left: draggableOffset.left });
+                    snapGuideline.css({
+                        "top": guidePosition.top,
+                        "left": guidePosition.left,
+                        "height": Math.abs(Math.max(snappedToOffset.top, draggableOffset.top) - Math.min(snappedToOffset.top + snappedToSize.height, draggableOffset.top + data.dragElementSize.height))
+                    });
+                    snapGuideline.addClass("draggable-snap-guidelines-vertical");
+                    break;
+                case "right":
+                    guidePosition = data._convertPositionTo("relative", { top: Math.min(snappedToOffset.top + snappedToSize.height, draggableOffset.top + data.dragElementSize.height), left: draggableOffset.left + data.dragElementSize.width });
+                    snapGuideline.css({
+                        "top": guidePosition.top,
+                        "left": guidePosition.left,
+                        "height": Math.abs(Math.max(snappedToOffset.top, draggableOffset.top) - Math.min(snappedToOffset.top + snappedToSize.height, draggableOffset.top + data.dragElementSize.height))
+                    });
+                    snapGuideline.addClass("draggable-snap-guidelines-vertical");
+                    break;
+                default:
+                    break;
+            }
+        });
+        if (options.guidelines.snap)
+        {
+            options.guidelines.snap.call(this, snaps, event.target);
+        }
+    },
+    stop: function (event, ui)
+    {
+        $(".draggable-snap-guidelines").remove();
+    }
+});
---
+++ jquery.ui.rotatable.js
@@ -0,0 +1,320 @@
+﻿(function($)
+{
+    $.widget("ui.rotatable", $.ui.mouse, {
+        version: "1.10.0",
+        widgetEventPrefix: "rotate",
+        options: {
+            addClasses: true,
+            appendTo: "parent",
+            cursor: "auto",
+            handle: false,
+            helper: "original",
+            opacity: "false",
+           
+            //callbacks
+            rotate: null,
+            start: null,
+            stop: null
+        },
+        _create: function()
+        {
+            if (this.options.addClasses)
+            {
+                this.element.addClass("ui-rotatable");
+            }
+            if (this.options.disabled)
+            {
+                this.element.addClass("ui-rotatable-disabled");
+            }
+
+            this.handles = this.options.handles || (!$('.ui-rotatable-handle', this.element).length ? "e,s,se" : { n: '.ui-rotatable-n', e: '.ui-rotatable-e', s: '.ui-rotatable-s', w: '.ui-rotatable-w', se: '.ui-rotatable-se', sw: '.ui-rotatable-sw', ne: '.ui-rotatable-ne', nw: '.ui-rotatable-nw' });
+            if (this.handles.constructor == String)
+            {
+
+                if (this.handles == 'all') this.handles = 'n,e,s,w,se,sw,ne,nw';
+                var n = this.handles.split(","); this.handles = {};
+
+                for (var i = 0; i < n.length; i++)
+                {
+
+                    var handle = $.trim(n[i]), hname = 'ui-rotatable-' + handle;
+                    var axis = $('<div class="ui-rotatable-handle ui-resizable-handle ' + hname + '"></div>');
+
+                    // Apply zIndex to all handles - see #7960
+                    axis.css({ zIndex: this.options.zIndex });
+
+                    //Insert into internal handles object and append to element
+                    this.handles[handle] = '.ui-rotatable-' + handle;
+                    this.element.append(axis);
+                }
+            }
+
+            this._mouseInit();
+        },
+       
+        _destroy: function()
+        {
+            this.element.removeClass("ui-rotatable ui-rotatable-rotating ui-rotatable-disabled");
+            this._mouseDestroy();
+        },
+       
+        _mouseCapture: function(event)
+        {
+            var o = this.options;
+
+            var handle = false;
+            if (o.handles)
+            {
+                for (var i in this.handles)
+                {
+                    if ($(this.handles[i])[0] == event.target)
+                    {
+                        handle = true;
+                    }
+                }
+            }
+            else
+            {
+                handle = true;
+            }
+
+            return !this.options.disabled && handle;
+
+            return true;
+        },
+       
+        _mouseStart: function(event)
+        {
+            var o = this.options;
+
+            this.helper = this._createHelper(event);
+
+            this.helper.addClass("ui-rotatable-rotating");
+
+            this._cacheHelperAngle();
+
+            this.originalAngle = this.angle = this._getRotationAngle();
+
+            if (this._trigger("start", event) === false)
+            {
+                this._clear();
+                return false;
+            }
+
+            this._cacheHelperProportions();
+
+            this._cacheCenterPoint();
+
+            this.mouseStartPosition = { 'x': event.pageX, 'y': event.pageY };
+
+            this._mouseDrag(event, true);
+
+            return true;
+        },
+        
+        _mouseDrag: function(event, noPropagation)
+        {
+            this.angle = parseInt(this._generateAngle(event) * (180 / Math.PI) - 90);
+
+            if (!noPropagation)
+            {
+                var ui = this._uiHash();
+                if (this._trigger("rotate", event, ui) === false)
+                {
+                    this._mouseUp({});
+                    return false;
+                }
+            }
+
+            var me = this;
+
+            var setIEAngle = function (angle)
+            {
+                //fix IE8 bug where handles stay unrotated. This helps, but only partually
+                //any part of the handles that are outside of the rotatable element
+                //will be cut off. Might need to wrap element into container and rotate container
+                if (me.helper.css("z-index") === "auto")
+                {
+                    me.helper.css("z-index", 1);
+                }
+                this.helper = me.helper[0];
+                var rad = Math.PI / 180
+                this._angle = angle;
+                    
+                var _rad = angle * rad;
+                costheta = Math.cos(_rad);
+                sintheta = Math.sin(_rad);
+                this.helper.style.filter = "progid:DXImageTransform.Microsoft.Matrix(M11='1.0', sizingmethod='auto expand')";
+                var fil = this.helper.filters.item("DXImageTransform.Microsoft.Matrix");
+                fil.M11 = costheta;
+                fil.M12 = -sintheta;
+                fil.M21 = sintheta;
+                fil.M22 = costheta;
+                this.helper.style.marginLeft = -(this.helper.offsetWidth - this.helper.clientWidth) / 2 + "px";
+                this.helper.style.marginTop = -(this.helper.offsetHeight - this.helper.clientHeight) / 2 + "px";
+            };
+
+            var prefixes = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' ');
+            var supportTranform = false;
+            for (var i = 0; i < prefixes.length; i++)
+            {
+                if (document.createElement('div').style[prefixes[i]] !== undefined)
+                {
+                    supportTranform = true;
+                    break;
+                }
+            }
+
+
+            if (!supportTranform)
+            {
+                setIEAngle(this.angle);
+            }
+
+            //set angle
+            this.helper.css({
+                '-webkit-transform': 'rotate(' + this.angle + 'deg)',
+                '-moz-transform': 'rotate(' + this.angle + 'deg)',
+                '-ms-transform': 'rotate(' + this.angle + 'deg)',
+                '-o-transform': 'rotate(' + this.angle + 'deg)',
+                'transform': 'rotate(' + this.angle + 'deg)'
+            });
+
+            return false;
+        },
+        
+        _mouseStop: function(event)
+        {
+            if (this._trigger("stop", event) !== false)
+            {
+                this._clear();
+            }
+            return false;
+        },
+        
+        _mouseUp: function(event)
+        {
+            return $.ui.mouse.prototype._mouseUp.call(this, event);
+        },
+        
+        _getHandle: function(event)
+        {
+            var handle = !this.options.handles || !$(this.options.handles, this.element).length ? true : false;
+            $(this.options.handles, this.element).find("*").addBack().each(function() {
+                if (this === event.target)
+                {
+                    handle = true;
+                }
+            });
+
+            return handle;
+        },
+        
+        _createHelper: function(event)
+        {
+            var o = this.options,
+                helper = $.isFunction(o.helper) ? $(o.helper.apply(this.element[0], [event])) : (o.helper === "clone" ? this.element.clone().removeAttr("id") : this.element);
+
+            if(!helper.parents("body").length) {
+                helper.appendTo((o.appendTo === "parent" ? this.element[0].parentNode : o.appendTo));
+            }
+
+            if(helper[0] !== this.element[0] && !(/(fixed|absolute)/).test(helper.css("position"))) {
+                helper.css("position", "absolute");
+            }
+
+            return helper;
+        },
+        
+        _cacheHelperProportions: function() {
+            this.helperProportions = {
+                width: this.helper.outerWidth(),
+                height: this.helper.outerHeight()
+            };
+        },
+
+        _cacheCenterPoint: function ()
+        {
+            var helperOffset = this.helper.offset();
+            this.centerPoint = {
+                x: helperOffset.left + this.helperProportions.width / 2,
+                y: helperOffset.top + this.helperProportions.height / 2
+            };
+        },
+
+        _cacheHelperAngle: function() {
+            this.helperAngle = this._getRotationAngle();
+        },
+
+        _generateAngle: function(event) {
+            //this is where we figure out new angle
+            var mouse_coords = { 'x': event.pageX, 'y': event.pageY };
+            return angle = this._positionToAngle(mouse_coords);
+        },
+        
+        _clear: function() {
+            this.helper.removeClass("ui-rotatable-rotating");
+            if(this.helper[0] !== this.element[0] && !this.cancelHelperRemoval) {
+                this.helper.remove();
+            }
+            this.helper = null;
+            this.cancelHelperRemoval = false;
+        },
+
+        // From now on bulk stuff - mainly helpers
+
+        _trigger: function(type, event, ui) {
+            ui = ui || this._uiHash();
+            $.ui.plugin.call(this, type, [event, ui]);
+            //The absolute position has to be recalculated after plugins
+            if(type === "rotate") {
+                //this.positionAbs = this._convertPositionTo("absolute");
+            }
+            return $.Widget.prototype._trigger.call(this, type, event, ui);
+        },
+
+        _positionToAngle: function(position) {
+            var x = position.x - this.centerPoint.x,
+                y = position.y - this.centerPoint.y,
+                hyp = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)),
+                angle = Math.acos(x / hyp);
+            if (y < 0)
+            {
+                angle = 2 * Math.PI - angle;
+            }
+            return angle;
+        },
+
+        plugins: {},
+
+        _uiHash: function() {
+            return {
+                helper: this.helper,
+                angle: this.angle,
+                originalAngle: this.originalAngle
+            };
+        },
+
+        _getRotationAngle: function()
+        {
+            var angle;
+            var matrix = this.helper.css("-webkit-transform") ||
+                this.helper.css("-moz-transform") ||
+                this.helper.css("-ms-transform") ||
+                this.helper.css("-o-transform") ||
+                this.helper.css("transform");
+            if (matrix && matrix !== 'none')
+            {
+                var values = matrix.split('(')[1].split(')')[0].split(',');
+                var a = values[0];
+                var b = values[1];
+                angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+            }
+            else
+            {
+                angle = 0;
+            }
+            return angle;
+        }
+    });
+})(jQuery);
